5828f16775fe4af47ed61f7249d6f6987a9c2f2e
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-10-13 18:32:05 +0300

    configrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/navigation/Layout";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
